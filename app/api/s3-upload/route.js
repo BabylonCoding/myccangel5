2f584892fdefc9f096122259ccc4306278133c33
BabylonCoding <124358490+BabylonCoding@users.noreply.github.com>
2025-01-14 05:43:34 +0000

    trying typescript

--- app/api/s3-upload/route.js
+++ app/api/s3-upload/route.js
@@ -2,7 +2,8 @@ import { NextResponse } from "next/server";
 import {S3Client, PutObjectCommand, GetObjectCommand} from "@aws-sdk/client-s3"
 import { error } from "console";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-
+import { Amplify } from "aws-amplify";
+Amplify.configure(outputs);
 //import UploadFile from "@/app/UploadFile";
 
 
--- app/components/S3UploadForm.jsx
+++ app/components/S3UploadForm.jsx
@@ -2,6 +2,8 @@
 import { useState } from "react";
 import {S3Client, PutObjectCommand, GetObjectCommand} from "@aws-sdk/client-s3"
  import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+ import { Amplify } from "aws-amplify";
+ Amplify.configure(outputs);
 
   const  S3UploadForm=()=>{
 //const UploadForm () =>{
