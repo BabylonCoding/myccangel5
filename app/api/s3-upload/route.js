6f0e40116310e1ce744cea74519f3a98e47926d5
BabylonCoding <124358490+BabylonCoding@users.noreply.github.com>
2025-01-14 05:58:00 +0000

    trying typescript

--- app/api/s3-upload/route.js
+++ app/api/s3-upload/route.js
@@ -3,7 +3,7 @@ import {S3Client, PutObjectCommand, GetObjectCommand} from "@aws-sdk/client-s3"
 import { error } from "console";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { Amplify } from "aws-amplify";
-Amplify.configure(outputs);
+Amplify.configure();
 //import UploadFile from "@/app/UploadFile";
 
 
